e5b5051169b8d562d5252bd78ff8ae776f5b839b
ChristinaAjemyan <kristin07.96@gmail.com>
2017-10-09 00:35:24 +0400

    create and remove buttons

--- app/controllers/tables-ctrl.js
+++ app/controllers/tables-ctrl.js
@@ -1,5 +1,30 @@
 App.controller('tablesCtrl',function($scope){
 	$scope.model = tableModel;
-	// $scope.getPrivateIcon = getPrivateIcon;
+	$scope.funForButton=function(arg) {
+		console.log(arg.users_list)
 
+	}
+	$scope.user_id=users[0].id;
+	$scope.createNewLine=function (event) {
+	event.preventDefault();
+	var name=document.getElementById('tableName').value;
+	var maxUsers=document.getElementById('maxUsers').value;
+	var private=document.getElementById("private").checked;
+	var newObject={
+		id: Math.random().toString(36).slice(2, 13),
+		table_name: name,
+		order: tableModel.length+1,
+		min_user_count: 2,
+		max_user_count: maxUsers,
+		is_private: private,
+		status: "waiting",
+		users_list: [],
+	}
+	tableModel.push(newObject)
+	}
+	$scope.remover=function(event, elem) {
+	event.preventDefault();
+	var td=event.target.parentNode;
+	td.parentNode.style.display="none"
+}
 })
