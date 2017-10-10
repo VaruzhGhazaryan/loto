09925f340c2bb912fca0a3bc395637e6ad60d8d3
Tigran <tigran.martirosian.1994.2016@gmail.com>
2017-10-10 23:14:48 +0400

    added myProject LOTO

--- app/controllers/tables-ctrl.js
+++ app/controllers/tables-ctrl.js
@@ -1,5 +1,26 @@
 App.controller('tablesCtrl',function($scope){
 	$scope.model = tableModel;
-	// $scope.getPrivateIcon = getPrivateIcon;
+	$scope.removeTab = function () {
+		var index= (tableModel.indexOf(this.game));
+		if (index >= 0) {
+			tableModel.splice(index, 1);
+		}
 
-})
+	};
+
+	$scope.addGames= function (name,max) {
+		if(name!=undefined && max <= 4) {
+			var obj = {
+				id: 't872esd92ekw9',
+				table_name: name,
+				order: 1, 
+				min_user_count: 2,
+				max_user_count: max,
+				is_private: true,
+				status: 'waiting',
+				users_list: ['9uew9uf3', '9uew9uf3', '9uew9uf3']
+			};
+			tableModel.push(obj)
+		}
+	}
+});
--- app/models/tables-model.js
+++ app/models/tables-model.js
@@ -4,7 +4,7 @@ var tableModel = [
 				table_name: 'loto1',  //table name [type: String]
 				order: 1, 			  //table position [type: Number]
 				min_user_count: 2,	  //minimum user count on game [type: Number]
-				max_user_count: 10,	  //maximum user count on game [type: Number]
+				max_user_count: 4,	  //maximum user count on game [type: Number]
 				is_private: false,	  //game is private or public  [type: Boolean]
 				status: 'waiting',    //game status {'waiting', 'started', 'finished'} [type: String]
 				users_list: ['9uew9uf3','9uew9uf3','9uew9uf3']	  	  //connected on game users list [type: Array] 
@@ -14,7 +14,7 @@ var tableModel = [
 				table_name: 'loto2',  //table name [type: String]
 				order: 2, 			  //table position [type: Number]
 				min_user_count: 3,	  //minimum user count on game [type: Number]
-				max_user_count: 9,	  //maximum user count on game [type: Number]
+				max_user_count: 4,	  //maximum user count on game [type: Number]
 				is_private: true,	  //game is private or public  [type: Boolean]
 				status: 'started',    //game status {'waiting', 'started', 'finished'} [type: String]
 				users_list: ['9uew9uf3']	  	  //connected on game users list [type: Array] 
@@ -24,9 +24,41 @@ var tableModel = [
 				table_name: 'loto3',  //table name [type: String]
 				order: 3, 			  //table position [type: Number]
 				min_user_count: 4,	  //minimum user count on game [type: Number]
-				max_user_count: 12,	  //maximum user count on game [type: Number]
+				max_user_count: 4,	  //maximum user count on game [type: Number]
 				is_private: false,	  //game is private or public  [type: Boolean]
 				status: 'finished',    //game status {'waiting', 'started', 'finished'} [type: String]
 				users_list: ['9uew9uf3','9uew9uf3','9uew9uf3','9uew9uf3','9uew9uf3']	  	  //connected on game users list [type: Array] 
 			}
-		]
+		];
+var userModel=[
+	{
+		id: 'sdf4534tr6yty', //uniq id,
+		firstname: 'John',
+		lastname: 'Doe',
+		email: 'example@mail.com',
+		password: '354rwet2413@#fdgdf', //password hash
+		gender: 'male', // male or female
+		coins: 150, // count of coins
+		photo_path: 'uploads/avatar.jpg' //user image path
+	},
+	{
+		id: 'sdf4534tr6yty', //uniq id,
+		firstname: 'John',
+		lastname: 'Doe',
+		email: 'example@mail.com',
+		password: '354rwet2413@#fdgdf', //password hash
+		gender: 'male', // male or female
+		coins: 150, // count of coins
+		photo_path: 'uploads/avatar.jpg' //user image path
+	},
+	{
+		id: 'sdf4534tr6yty', //uniq id,
+		firstname: 'John',
+		lastname: 'Doe',
+		email: 'example@mail.com',
+		password: '354rwet2413@#fdgdf', //password hash
+		gender: 'male', // male or female
+		coins: 150, // count of coins
+		photo_path: 'uploads/avatar.jpg' //user image path
+	}
+]
