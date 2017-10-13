9876a39eae3acae809fcf00093fd353e26315174
K3LSD <00rs@bk.ru>
2017-10-13 17:37:38 +0400

    added new features

--- app/controllers/tables-ctrl.js
+++ app/controllers/tables-ctrl.js
@@ -1,5 +1,41 @@
 App.controller('tablesCtrl',function($scope){
 	$scope.model = tableModel;
-	// $scope.getPrivateIcon = getPrivateIcon;
 
-})
+
+    for(var i = 0; i < $scope.model.length; i++){
+        var result = $scope.model[i];
+        if(result['is_private'] === true){
+            result.icon = 'fa fa-lock fa-1.5x';
+            result.is_private = "disableBtn = 'true'";
+        }else{
+            result.icon = 'fa fa-unlock fa-1.5x'
+        }
+        if(result['status'] === 'waiting'){
+            result.status = 'waiting';
+        }else if(result['status'] === 'started'){
+            result.status = 'started';
+        }else if(result['status'] === 'finished'){
+            result.status = 'finished'
+        }
+    }
+
+
+
+
+
+    $scope.addNew = function(userModel){
+
+        $scope.model.push({
+            'table_name': userModel.table_name,
+            'status' : userModel.status = 'waiting',
+            'is_private' : userModel.icon,
+            'min_user_count' :userModel.min_user_count,
+            'max_user_count' :userModel.max_user_count
+        });
+    };
+
+    $scope.deleteUser = function (user) {
+        var index = $scope.model.indexOf(user);
+        $scope.model.splice(index, 1);
+    };
+});
--- app/models/tables-model.js
+++ app/models/tables-model.js
@@ -1,32 +1,32 @@
 var tableModel = [
-			{
-				id: 't872esd92ekw9',  //unicue id [type: String]
-				table_name: 'loto1',  //table name [type: String]
-				order: 1, 			  //table position [type: Number]
-				min_user_count: 2,	  //minimum user count on game [type: Number]
-				max_user_count: 10,	  //maximum user count on game [type: Number]
-				is_private: false,	  //game is private or public  [type: Boolean]
-				status: 'waiting',    //game status {'waiting', 'started', 'finished'} [type: String]
-				users_list: ['9uew9uf3','9uew9uf3','9uew9uf3']	  	  //connected on game users list [type: Array] 
-			},
-			{
-				id: 't872esb92ekw9',  //unicue id [type: String]
-				table_name: 'loto2',  //table name [type: String]
-				order: 2, 			  //table position [type: Number]
-				min_user_count: 3,	  //minimum user count on game [type: Number]
-				max_user_count: 9,	  //maximum user count on game [type: Number]
-				is_private: true,	  //game is private or public  [type: Boolean]
-				status: 'started',    //game status {'waiting', 'started', 'finished'} [type: String]
-				users_list: ['9uew9uf3']	  	  //connected on game users list [type: Array] 
-			},
-			{
-				id: 't872esd02ekw9',  //unicue id [type: String]
-				table_name: 'loto3',  //table name [type: String]
-				order: 3, 			  //table position [type: Number]
-				min_user_count: 4,	  //minimum user count on game [type: Number]
-				max_user_count: 12,	  //maximum user count on game [type: Number]
-				is_private: false,	  //game is private or public  [type: Boolean]
-				status: 'finished',    //game status {'waiting', 'started', 'finished'} [type: String]
-				users_list: ['9uew9uf3','9uew9uf3','9uew9uf3','9uew9uf3','9uew9uf3']	  	  //connected on game users list [type: Array] 
-			}
-		]
+    {
+        id: 't872esd92ekw9',
+        table_name: 'loto1',
+        order: 1,
+        min_user_count: 2,
+        max_user_count: 10,
+        is_private: false,
+        status: 'waiting',
+        users_list: ['9uew9uf3', '9uew9uf3', '9uew9uf3']
+    },
+    {
+        id: 't872esb92ekw9',
+        table_name: 'loto2',
+        order: 2,
+        min_user_count: 4,
+        max_user_count: 9,
+        is_private: true,
+        status: 'started',
+        users_list: ['9uew9uf3']
+    },
+    {
+        id: 't872esd02ekw9',
+        table_name: 'loto3',
+        order: 3,
+        min_user_count: 4,
+        max_user_count: 12,
+        is_private: false,
+        status: 'finished',
+        users_list: ['9uew9uf3', '9uew9uf3', '9uew9uf3', '9uew9uf3', '9uew9uf3']
+    }
+];
